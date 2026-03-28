481947391f741f448f7edf1005a94f5367736a79
bhaveshhhhhhhhh <152094398+bhaveshhhhhhhhh@users.noreply.github.com>
2026-03-28 11:24:17 +0530

    Create App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="background">
+      <div className="overlay">
+        <div>
+          <h1>Welcome to Paradise Nursery</h1>
+          <p>Your one-stop shop for beautiful plants</p>
+
+          <button style={{ padding: "10px 20px", fontSize: "16px" }}>
+            Get Started
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
